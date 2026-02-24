11dfbf22efe6834d55f6e908db960a640f2f3c90
israel.barcelos@sebratel.com.br <israel.barcelos@corp.sebratel.com.br>
2026-02-24 09:27:09 -0300

    adicao de estrutura basica para consumo de download por usuario

--- src/main/java/br/com/sebratel/bff/controller/ConsumoController.java
+++ src/main/java/br/com/sebratel/bff/controller/ConsumoController.java
@@ -1,13 +1,16 @@
 package br.com.sebratel.bff.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import br.com.sebratel.bff.dto.ConsumoDTO;
 import br.com.sebratel.bff.service.ConsumoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 
@@ -32,4 +35,12 @@ public class ConsumoController {
     public ResponseEntity<List<ConsumoDTO>> getConsumoAlto() {
         return ResponseEntity.ok(service.listarConsumoAlto());
     }
+
+    @GetMapping("/consumo-alto-paginado")
+    public ResponseEntity<Page<ConsumoDTO>> getConsumoAlto(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size) {
+        return ResponseEntity.ok(service.listarConsumoAltoPaginado(page, size));
+    }
+
 }
--- src/main/java/br/com/sebratel/bff/repository/erp/PlanoRepository.java
+++ src/main/java/br/com/sebratel/bff/repository/erp/PlanoRepository.java
@@ -4,6 +4,7 @@ import br.com.sebratel.bff.model.ErpContract;
 import br.com.sebratel.bff.repository.erp.projections.PlanoProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -22,4 +23,19 @@ public interface PlanoRepository extends JpaRepository<ErpContract, Long> {
         LEFT JOIN erp.people p ON p.id = c.client_id
         """, nativeQuery = true)
     List<PlanoProjection> findTodosPlanos();
+
+    @Query(value = """
+        SELECT 
+            ac.user AS username, 
+            sp.title AS plano, 
+            c.contract_number AS contrato, 
+            p.name AS cliente 
+        FROM erp.authentication_contracts ac 
+        LEFT JOIN erp.service_products sp ON sp.id = ac.service_product_id 
+        LEFT JOIN erp.contracts c ON c.id = ac.contract_id 
+        LEFT JOIN erp.people p ON p.id = c.client_id
+        WHERE ac.user IN (:usernames)
+        """, nativeQuery = true)
+    List<PlanoProjection> findPlanosPorUsernames(@Param("usernames") List<String> usernames);
+
 }
