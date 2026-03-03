ca519d02ce369c94b82feafaab944942d82f899f
Israel da Rosa Barcelos <israel.barcelos@sebratel.com.br>
2026-03-03 14:50:27 -0300

    melhorando a configuração do webclient para o caso de criação de massivas

---
+++ src/main/java/br/com/sebratel/bff/dto/TokenResponseDTO.java
@@ -0,0 +1,14 @@
+package br.com.sebratel.bff.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class TokenResponseDTO {
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("expires_in")
+    private int expiresIn;
+    @JsonProperty("token_type")
+    private String tokenType;
+}
