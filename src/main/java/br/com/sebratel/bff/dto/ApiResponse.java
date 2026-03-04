d25415a454a01ec997631816c41b7be110731886
Israel da Rosa Barcelos <israel.barcelos@sebratel.com.br>
2026-03-04 18:14:56 -0300

    Adição de apiresponse para respostas

---
+++ src/main/java/br/com/sebratel/bff/dto/ApiResponse.java
@@ -0,0 +1,18 @@
+package br.com.sebratel.bff.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private final boolean success;
+    private final String message;
+    private final T data;
+    private final Object details;
+    private final LocalDateTime timestamp = LocalDateTime.now();
+}
