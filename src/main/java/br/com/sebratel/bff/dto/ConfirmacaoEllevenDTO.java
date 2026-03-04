4994889d0a5f43cf137797dc7dc1f48e367b51af
Israel da Rosa Barcelos <israel.barcelos@sebratel.com.br>
2026-03-04 18:14:21 -0300

    Alteração nos contratos de massivas

--- src/main/java/br/com/sebratel/bff/dto/ConfirmacaoEllevenDTO.java
+++ src/main/java/br/com/sebratel/bff/dto/ConfirmacaoEllevenDTO.java
@@ -1,12 +1,16 @@
 package br.com.sebratel.bff.dto;
 
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ConfirmacaoEllevenDTO {
-    private String success;
+    private boolean success;
+    @Nullable
+    private String message;
 }
