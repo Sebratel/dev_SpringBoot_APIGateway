07856bb033cf7ccf8c6736a377392d36d0b61a5d
Israel da Rosa Barcelos <israel.barcelos@sebratel.com.br>
2026-04-02 17:05:58 -0300

    (feat) now if there is any error of not found, it sends

--- src/main/java/br/com/sebratel/bff/repository/erp/PersonRepository.java
+++ src/main/java/br/com/sebratel/bff/repository/erp/PersonRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
-    PersonEntity findByTxId(String cpf);
+    Optional<PersonEntity> findByTxId(String cpf);
 
     @Query(value = """
         SELECT 
@@ -18,5 +20,5 @@ public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
         INNER JOIN authentication_contracts ac on ac.contract_id = c.id
         WHERE p.tx_id = :cpf
         """, nativeQuery = true)
-    ContractProjection findContractByCPF(String cpf);
+    Optional<ContractProjection> findContractByCPF(String cpf);
 }
