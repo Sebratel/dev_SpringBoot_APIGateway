f5a87841dbcb22f613229e386801fff956139b55
Israel da Rosa Barcelos <israel.barcelos@sebratel.com.br>
2026-04-08 18:19:02 -0300

    (feat) logic for null items

--- src/main/java/br/com/sebratel/bff/dto/CreateImpactedUsersInputDTO.java
+++ src/main/java/br/com/sebratel/bff/dto/CreateImpactedUsersInputDTO.java
@@ -1,6 +1,7 @@
 package br.com.sebratel.bff.dto;
 
 import br.com.sebratel.bff.model.entity.UsuarioAfetadoEntity;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,8 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateImpactedUsersInputDTO {
+    @NotNull(message = "Usuários devem ser adicionados")
     List<UsuarioAfetadoEntity> usuarioAfetadoEntities;
+    @NotNull(message = "AssignmentId é Obrigatório")
     Long assignmentId;
 }
