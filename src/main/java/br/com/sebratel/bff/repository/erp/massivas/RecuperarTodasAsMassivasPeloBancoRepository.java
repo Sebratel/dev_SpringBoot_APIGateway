9d4876f00afdfa4c56d86f7d096f3496c5812361
Israel da Rosa Barcelos <israel.barcelos@sebratel.com.br>
2026-04-30 11:09:02 -0300

    (feature) Adding Finalized events to sorting

--- src/main/java/br/com/sebratel/bff/repository/erp/massivas/RecuperarTodasAsMassivasPeloBancoRepository.java
+++ src/main/java/br/com/sebratel/bff/repository/erp/massivas/RecuperarTodasAsMassivasPeloBancoRepository.java
@@ -36,7 +36,7 @@ public interface RecuperarTodasAsMassivasPeloBancoRepository extends JpaReposito
                 left join catalog_services cs on cs.id = ai.catalog_service_id
                 left join authentication_access_points aap on aap.id = ai.authentication_access_point_id
             where it.id in (1176, 302, 1257, 1265)
-                and t2.title not in ('Encerrado', 'Cancelado')
+                and t2.title not in ('Cancelado')
             order by id desc
             """, nativeQuery = true)
     List<RecuperarTodasAsMassivasProjection> findActiveAssignments();
